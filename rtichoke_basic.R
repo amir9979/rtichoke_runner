fffa991bccc4a12895f5aeca58bf8a49b01686a7
Amir Elmishali <amir9979@gmail.com>
2022-09-19 12:30:04 +0300

    Update rtichoke_basic.R

--- rtichoke_basic.R
+++ rtichoke_basic.R
@@ -1,4 +1,7 @@
 library(rtichoke)
 predictions <- read.csv(file = 'classes/prediction.csv')
+print(typeof(predictions))
+print(typeof(predictions['True_probability']))
+print(typeof(list(predictions['True_probability'])))
 create_summary_report(probs = list(predictions['True_probability']), 
                       reals= list(predictions['actual']))
