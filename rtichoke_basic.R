4e500e96897acdc53c9b86debbb7d376ceb8a9df
Amir Elmishali <amir9979@gmail.com>
2022-09-19 15:44:07 +0300

    fix true false to nums

--- rtichoke_basic.R
+++ rtichoke_basic.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(rtichoke)
-predictions <- read.csv(file = 'classes/prediction.csv', sep=';')
-actual <- map(predictions$actual, function(x) ifelse(x=='False',0,1))
-print(str(actual)
-sum(actual[1])
+predictions <- read.csv(file = 'classes/prediction_dummy.csv', sep=';')
+# actual <- map(predictions$actual, function(x) ifelse(x=='False',0,1))
+sum(list(predictions$actual[1]))
+# sum(predictions$actual[1])
 create_summary_report(probs = list(predictions$True_probability), 
-                      reals= actual)
+                      reals= list(predictions$actual))
