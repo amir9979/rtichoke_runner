7a7edcb515421fdcac4c7f10d1120eda8a397fc9
Amir Elmishali <amir9979@gmail.com>
2022-09-20 16:09:53 +0300

    Update rtichoke_basic.R

--- rtichoke_basic.R
+++ rtichoke_basic.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(rtichoke)
-predictions0 <- read.csv(file = 'results/0.csv', sep=';')
-predictions1 <- read.csv(file = 'results/1.csv', sep=';')
-create_summary_report(probs = list( '0' = predictions0$True_probability, '1' = predictions1$False_probability),
-                      reals= list(predictions0$actual), output_file= 'summary_report.html', output_dir  = '../pages')
+reals = list()
+probs = list()
+csvs = list.files(path='results'), pattern= '*.csv')
+for (i in 1:length(temp)) 
+  {
+  p = read.csv(file = csvs[i], sep=';')
+  reals = list(p$actual)
+  probs[i] = list(p$True_probability)
+   }
+create_summary_report(probs = probs,
+                      reals= reals, output_dir  = '../pages')
