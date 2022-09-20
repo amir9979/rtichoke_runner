9cf2bb51568f953e95ed35a91c6bfb9746cfdf46
Amir Elmishali <amir9979@gmail.com>
2022-09-20 11:01:58 +0300

    Update rtichoke_basic.R

--- rtichoke_basic.R
+++ rtichoke_basic.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(rtichoke)
 predictions <- read.csv(file = 'classes/prediction_dummy.csv', sep=';')
-print(names(predictions))
-create_summary_report(probs = list(predictions$X0_probability), 
+create_summary_report(probs = list(predictions$True_probability), 
                       reals= list(predictions$actual))
