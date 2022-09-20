9b7b0a2289ff14630ed3433c8bf1f933584a7d04
Amir Elmishali <amir9979@gmail.com>
2022-09-20 11:26:54 +0300

    Delete rtichoke_basic.R

--- rtichoke_basic.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-library(rtichoke)
-predictions <- read.csv(file = 'classes/prediction_dummy.csv', sep=';')
-create_summary_report(probs = list(predictions$True_probability), 
-                      reals= list(predictions$actual))
